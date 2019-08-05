8d3c960ccac8ef44a508830b22264e636c6d069b
root <abrahamdimma@gmail.com>
2019-08-05 17:31:17 +0000

    update commit

--- wls.sh
+++ wls.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp /u01/udemy/Oracle/Middleware/Oracle_Home/wlserver/server/lib/weblogic.jar weblogic.Deployer -verbose -noexit -adminurl t3://54.156.62.113:7001/console/login/LoginForm.jsp -user $1 -password $2 -name mainWebApp -stop  -timeout 300
+java -cp /u01/udemy/Oracle/Middleware/Oracle_Home/wlserver/server/lib/weblogic.jar weblogic.Deployer -verbose -noexit -adminurl t3://54.156.62.113:7001/console/login/LoginForm.jsp -user $1 -password $2 -name mainWebApp -undeploy  -timeout 300
